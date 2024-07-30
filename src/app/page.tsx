24fa2a8f92543dfb3ef50b27668c973f499d61fe
James Kevin Jamili <jameskevin.ramesessystems@gmail.com>
2024-07-30 12:09:49 +0800

    updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
   const [data, setData] = useState<null | any>(null);
   const [error, setError] = useState<any | null>(null);
 
-  const handleClick = async () => {
+  const fetchData = async () => {
     const res = await svc?.invoke("getExampleData");
     if (!res || res.error) {
       setError(res.error);
@@ -17,16 +17,45 @@ export default function Home() {
     }
   };
 
+  const createData = async () => {
+    const res = await svc?.invoke("createExampleData");
+    return res;
+  };
+
+  const updateData = async () => {
+    const res = await svc?.invoke("updateExampleData");
+    return res;
+  };
+
+  const deleteData = async () => {
+    const res = await svc?.invoke("deleteExampleData");
+    return res;
+  };
+
   return (
-    <div>
+    <div className="pl-2">
       Home
       {error && <h1>{error.errorResponse.errmsg}</h1>}
       {data?.map((datas: any) => (
         <div key={datas._id}>
-          {datas.title} - {datas.number}
+          {datas.id} - {datas.title} - {datas.number}
         </div>
       ))}
-      <button onClick={handleClick}>click</button>
+      <div className="flex gap-2">
+        <button onClick={fetchData} className="p-1 bg-green-200 rounded">
+          fetched
+        </button>
+        <button onClick={createData} className="p-1 bg-blue-200 rounded">
+          Insert
+        </button>
+
+        <button onClick={updateData} className="p-1 bg-yellow-200 rounded">
+          update
+        </button>
+        <button onClick={deleteData} className="p-1 bg-red-200 rounded">
+          delete
+        </button>
+      </div>
     </div>
   );
 }
--- src/services/exampleService.tsx
+++ src/services/exampleService.tsx
@@ -11,6 +11,43 @@ export const getExampleData = async () => {
   }
 };
 
+export const createExampleData = async () => {
+  try {
+    const db = openDb("test2", "todos");
+    const groups = await db.insert({
+      id: 3,
+      title: "wassup world",
+      number: 100,
+    });
+    return groups;
+  } catch (err) {
+    return { error: err };
+  }
+};
+
+export const updateExampleData = async () => {
+  try {
+    const db = openDb("test2", "todos");
+    const groups = await db.update(
+      { id: 1 },
+      { id: 1, title: "wassup world", number: 800 }
+    );
+    return groups;
+  } catch (err) {
+    return { error: err };
+  }
+};
+
+export const deleteExampleData = async () => {
+  try {
+    const db = openDb("test2", "todos");
+    const groups = await db.deleteMany({ id: 3 });
+    return groups;
+  } catch (err) {
+    return { error: err };
+  }
+};
+
 // export const getOccupancyTypeGroups = async () => {
 //   const db = openDb("cloud_obo", "occupancytype");
 //   const groups = await db.getList({}, { projection: { id: 1, title: 1 } });
